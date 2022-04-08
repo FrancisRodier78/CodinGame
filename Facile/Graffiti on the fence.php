c050a4a53ff89afdf674921813e327e0d46d6e5a
FrancisRodier78 <FrancisRodier78@users.noreply.github.com>
2022-04-08 17:36:19 +0200

    Update Graffiti on the fence.php

--- Facile/Graffiti on the fence.php
+++ Facile/Graffiti on the fence.php
@@ -2,9 +2,11 @@
 Objectif
 Vous êtes payé par un fermier pour peindre sa clôture.
 
-Sachant que la tâche peut être trop difficile pour vous, vous avez sous-traité le travail à une école d'étudiants comme activité parascolaire, pour dessiner des graffitis sur la clôture.
+Sachant que la tâche peut être trop difficile pour vous, vous avez sous-traité le travail à une école d'étudiants comme activité parascolaire, 
+pour dessiner des graffitis sur la clôture.
 
-Après une journée entière de travail et de plaisir, les élèves ont indiqué quelles sections de la clôture avaient été peintes. Les étudiants travaillaient en petites équipes ou individuellement, de sorte que vous obteniez plusieurs rapports séparés d'eux.
+Après une journée entière de travail et de plaisir, les élèves ont indiqué quelles sections de la clôture avaient été peintes. 
+Les étudiants travaillaient en petites équipes ou individuellement, de sorte que vous obteniez plusieurs rapports séparés d'eux.
 
 La clôture est marquée à chaque mètre, à partir du mètre 0. Une section peinte est signalée comme
 st ed
@@ -24,7 +26,8 @@ ed = 4
 longueur peinte = 3
 
 
-Les sections peintes peuvent ne pas être continues, réalisées sur différentes parties de la clôture. Certaines sections signalées se chevauchent même. Les rapports ne sont pas triés dans un ordre spécial.
+Les sections peintes peuvent ne pas être continues, réalisées sur différentes parties de la clôture. 
+Certaines sections signalées se chevauchent même. Les rapports ne sont pas triés dans un ordre spécial.
 
 Ayant les rapports en main, vous voulez savoir quelles sections de la clôture n'ont pas été peintes.
 -->
