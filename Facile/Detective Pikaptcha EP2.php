1b2926a8592b9512d439a3f397aa01b194a7faf9
FrancisRodier78 <FrancisRodier78@users.noreply.github.com>
2022-04-08 16:28:02 +0200

    Update Detective Pikaptcha EP2.php

--- Facile/Detective Pikaptcha EP2.php
+++ Facile/Detective Pikaptcha EP2.php
@@ -1,36 +1,40 @@
 <!--
 Objectif
 
-Grâce à votre aide, le détective Pikaptcha a une meilleure idée de l'endroit où il s'est fait emprisonné : c'est un labyrinthe spatio-déformé ! Pikaptcha sait que la sortie est invisible et qu'il va devoir cartographier la structure du labyrinthe pour pouvoir s'en sortir.
+Grâce à votre aide, le détective Pikaptcha a une meilleure idée de l'endroit où il s'est fait emprisonné : c'est un labyrinthe spatio-déformé ! 
+Pikaptcha sait que la sortie est invisible et qu'il va devoir cartographier la structure du labyrinthe pour pouvoir s'en sortir.
 
 «C'est l'heure de tester une bonne vieille méthode : suivre un mur et compter le nombre de fois que je passe sur chacune des cases.»
-Votre objectif est d'écrire un programme qui retournera pour chaque case du labyrinthe le nombre de fois que Pikaptcha traversera la case en suivant un mur jusqu'à atteindre la case de départ.
+Votre objectif est d'écrire un programme qui retournera pour chaque case du labyrinthe le nombre de fois 
+que Pikaptcha traversera la case en suivant un mur jusqu'à atteindre la case de départ.
+
 wall-following
 Parcours en suivant le mur de gauche
-  Règles
+Règles
 Le labyrinthe est rempli de 0 et de # ; ici 0 représente un passage et # représente un mur : une case infranchissable.
 La position et direction initiale de Pikaptcha sont données dans la grille avec un caractère spécial :
-
     >: face à la droite
     v: face vers le bas
     <: face à la gauche
     ^: face vers le haut 
 
 Un caractère supplémentaire indique quel mur Pikaptcha va suivre :
-
     R le mur à sa droite
     L le mur à sa gauche 
 
 On considère qu’une case a au maximum 4 voisins (les cases en diagonale ne sont pas adjacentes).
-Vous devez analyser le labyrinthe donné et le retourner avec une petite transformation : pour chaque case vide, au lieu de 0, retournez le nombre de fois que Pikaptcha a traversé cette case pendant sa traversée du labyrinthe, en suivant un mur. Pour chaque case mur, ne changez rien, retournez toujours #.
-  Note
+Vous devez analyser le labyrinthe donné et le retourner avec une petite transformation : 
+  pour chaque case vide, au lieu de 0, retournez le nombre de fois que Pikaptcha a traversé cette case pendant sa traversée du labyrinthe, en suivant un mur. 
+  Pour chaque case mur, ne changez rien, retournez toujours #.
 
+Note
 N'oubliez pas d'exécuter les tests depuis la fenêtre "Jeu de tests". Chaque test réussi rapporte un certain nombre de points (par exemple 10%).
 
-Attention : les tests fournis et les validateurs utilisés pour le calcul du score sont similaires mais différents (pas de points pour les solutions "hardcodées").
+Attention : les tests fournis et les validateurs utilisés pour le calcul du score sont similaires 
+mais différents (pas de points pour les solutions "hardcodées").
 
-Vous pouvez soumettre autant de solutions que vous le souhaitez. Seule votre meilleure soumission par question sera utilisée pour le classement final.
-
+Vous pouvez soumettre autant de solutions que vous le souhaitez. 
+Seule votre meilleure soumission par question sera utilisée pour le classement final.
 -->
 
 <?php
